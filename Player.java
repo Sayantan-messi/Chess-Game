2cbadab404880ff478ed80c6f7acf1ad14af228e
Sayantan Paul <59511017+Sayantan-messi@users.noreply.github.com>
2022-09-10 20:32:24 +0530

    Create Player.java
    Player: Number of players in the game

---
+++ Player.java
@@ -0,0 +1,31 @@
+public abstract class Player {
+	public boolean whiteSide;
+	public boolean humanPlayer;
+
+	public boolean isWhiteSide()
+	{
+		return this.whiteSide;
+	}
+	public boolean isHumanPlayer()
+	{
+		return this.humanPlayer;
+	}
+}
+
+class HumanPlayer extends Player {
+
+	public HumanPlayer(boolean whiteSide)
+	{
+		this.whiteSide = whiteSide;
+		this.humanPlayer = true;
+	}
+}
+
+class ComputerPlayer extends Player {
+
+	public ComputerPlayer(boolean whiteSide)
+	{
+		this.whiteSide = whiteSide;
+		this.humanPlayer = false;
+	}
+}
