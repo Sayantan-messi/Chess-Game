edd43b7627726064d31dea2bb1756b69d71a040b
Sayantan Paul <59511017+Sayantan-messi@users.noreply.github.com>
2022-09-10 20:56:48 +0530

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,15 +1,15 @@
 public class Board {
-	Spot[][] boxes;
+	static Spot[][] boxes;
 
 	public Board()
 	{
-		this.resetBoard();
+		Board.resetBoard();
 	}
 
 	public Spot getBox(int x, int y)
 	{
 
-		if (x < 0 || x > 5 || y < 0 || y > 5) {
+		if (x < 0 || x > 4 || y < 0 || y > 4) {
 			try {
         throw new Exception("Index out of bound");
       } catch (Exception e) {
@@ -21,24 +21,30 @@ public class Board {
 		return boxes[x][y];
 	}
 
-	public void resetBoard()
+	public static void resetBoard()
 	{
 		// initialize white pieces
-		boxes[0][0] = new Spot(0, 0, new Rook(true));
+		boxes[0][0] = new Spot(0, 0, new Rook("white"));
 		boxes[0][1] = new Spot(0, 1, new Knight(true));
-		boxes[0][2] = new Spot(0, 2, new Bishop(true));
+		boxes[0][2] = new Spot(0, 2, new Bishop("white"));
+		boxes[0][3] = new Spot(0, 3, new King(true));
 		//...
-		boxes[1][0] = new Spot(1, 0, new Pawn(true));
-		boxes[1][1] = new Spot(1, 1, new Pawn(true));
+		boxes[1][0] = new Spot(1, 0, new Pawn("white"));
+		boxes[1][1] = new Spot(1, 1, new Pawn("white"));
+		boxes[1][2] = new Spot(1, 2, new Pawn("white"));
+		boxes[1][3] = new Spot(1, 3, new Pawn("white"));
 		//...
 
 		// initialize black pieces
-		boxes[4][0] = new Spot(4, 0, new Rook(false));
+		boxes[4][0] = new Spot(4, 0, new Rook("black"));
 		boxes[4][1] = new Spot(4, 1, new Knight(false));
-		boxes[4][2] = new Spot(4, 2, new Bishop(false));
+		boxes[4][2] = new Spot(4, 2, new Bishop("black"));
+		boxes[4][3] = new Spot(4, 3, new King(false));
 		//...
-		boxes[3][0] = new Spot(3, 0, new Pawn(false));
-		boxes[3][1] = new Spot(3, 1, new Pawn(false));
+		boxes[3][0] = new Spot(3, 0, new Pawn("black"));
+		boxes[3][1] = new Spot(3, 1, new Pawn("black"));
+		boxes[3][2] = new Spot(3, 2, new Pawn("black"));
+		boxes[3][3] = new Spot(3, 3, new Pawn("black"));
 		//...
 
 		// initialize remaining boxes without any piece
@@ -47,5 +53,9 @@ public class Board {
 				boxes[i][j] = new Spot(i, j, null);
 			}
 		}
+	}
+
+	public static void main(String args[]) {
+		resetBoard();
 	}
 }
